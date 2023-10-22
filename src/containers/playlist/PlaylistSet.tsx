5ac22a20dad77f59354884b205c25dc393326fc2
bdeTheux <boris.detheux@student.vinci.be>
2023-10-22 10:05:37 +0200

    Add - select playlist in dropdown

--- src/containers/playlist/PlaylistSet.tsx
+++ src/containers/playlist/PlaylistSet.tsx
@@ -2,25 +2,36 @@ import { useSelector } from "react-redux";
 import { playlistsSelectors } from "./selectors";
 import { DownOutlined } from '@ant-design/icons';
 import { Dropdown, Space } from 'antd';
+import { SetStateAction, useState } from "react";
+import { Playlist } from "./slice";
 
 const PlaylistSet = () => {
-
+    const [selectedPlaylist, setSelectedPlaylist] = useState<Playlist>();
     const playlists = useSelector(playlistsSelectors.getPlaylists);
     console.log("pla", playlists)
     const items = playlists.map(item => ({
         key: "" + item.id,
-        label: item.name
+        label: item.name,
+        onClick: () => choicePlaylist(item)
       }));
+    const choicePlaylist = (playlist: Playlist) => {
+        if(playlist !== undefined){
+            setSelectedPlaylist(playlist);
+        }
+    }
     
     return (
-        <Dropdown menu={{items}} trigger={['click']}>
-            <a onClick={(e) => e.preventDefault()}>
-            <Space>
-                Click me
-                <DownOutlined />
-            </Space>
-            </a>
-        </Dropdown>
+        <div>
+            <Dropdown menu={{items}} trigger={['click']}>
+                <a onClick={(e) => choicePlaylist}>
+                <Space>
+                    {selectedPlaylist ? selectedPlaylist.name : "Choose a playlist"}
+                    <DownOutlined />
+                </Space>
+                </a>
+            </Dropdown>
+            <p>{selectedPlaylist ? selectedPlaylist.description : " "}</p>
+        </div>
     );
 
 }
